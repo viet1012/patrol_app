76f7cdea97198b645416f1fc37a9f5bd6a771c1b
viet.ta <viettta1012@gmail.com>
2025-12-16 16:16:28 +0700

    wip

--- lib/model/reason_model.dart
+++ lib/model/reason_model.dart
@@ -43,22 +43,22 @@ class RiskOption {
 }
 
 final List<RiskOption> frequencyOptions = [
-  RiskOption(labelKey: "frequency_often", score: 5),
-  RiskOption(labelKey: "frequency_sometimes", score: 3),
+  RiskOption(labelKey: "frequency_often", score: 4),
+  RiskOption(labelKey: "frequency_sometimes", score: 2),
   RiskOption(labelKey: "frequency_rare", score: 1),
 ];
 
 final List<RiskOption> probabilityOptions = [
-  RiskOption(labelKey: "probability_certain", score: 5),
+  RiskOption(labelKey: "probability_certain", score: 6),
   RiskOption(labelKey: "probability_high", score: 4),
-  RiskOption(labelKey: "probability_possible", score: 3),
+  RiskOption(labelKey: "probability_possible", score: 2),
   RiskOption(labelKey: "probability_rare", score: 1),
 ];
 
 final List<RiskOption> severityOptions = [
-  RiskOption(labelKey: "severity_critical", score: 5),
-  RiskOption(labelKey: "severity_severe", score: 4),
-  RiskOption(labelKey: "severity_moderate", score: 3),
-  RiskOption(labelKey: "severity_minor", score: 2),
+  RiskOption(labelKey: "severity_critical", score: 10),
+  RiskOption(labelKey: "severity_severe", score: 7),
+  RiskOption(labelKey: "severity_moderate", score: 5),
+  RiskOption(labelKey: "severity_minor", score: 3),
   RiskOption(labelKey: "severity_negligible", score: 1),
 ];
