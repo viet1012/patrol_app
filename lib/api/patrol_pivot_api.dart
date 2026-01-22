9b9abcbe61344dfdfc634a7bec803e0f477973e8
viet.ta <viettta1012@gmail.com>
2026-01-22 14:11:23 +0700

    22.1 update patrol user

--- lib/api/patrol_pivot_api.dart
+++ lib/api/patrol_pivot_api.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 import '../model/pivot_response.dart';
 import 'dio_client.dart';
 
@@ -5,11 +7,25 @@ class PatrolPivotApi {
   static Future<RiskPivotResponse> fetchPivot({
     required String plant,
     required List<String> atStatus,
+    required String type,
   }) async {
-    final endpoint = '/api/patrol_report/pivot'; // ví dụ endpoint
+    final endpoint = '/api/patrol_report/pivot';
+
+    final uri = Uri(
+      path: endpoint,
+      queryParameters: {
+        'plant': plant,
+        'type': type,
+        // List -> multi param
+        'at_status': atStatus,
+      },
+    );
+
+    debugPrint('➡️ REQUEST URL: ${uri.toString()}');
+
     final res = await DioClient.dio.get(
       endpoint,
-      queryParameters: {'plant': plant, 'at_status': atStatus},
+      queryParameters: {'plant': plant, 'type': type, 'at_status': atStatus},
     );
 
     if (res.statusCode == 200 && res.data is Map) {
