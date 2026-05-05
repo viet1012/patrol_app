7f2ea43ab7d894c860447e3fd1f775468fc0bf03
viet1012 <viet1012@users.noreply.github.com>
2026-05-05 09:03:32 +0000

    deploy: 929b65c4d7ac15d541077f01080b640b8eade88b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1667318860"
+    serviceWorkerVersion: "2661356271"
   }
 });
